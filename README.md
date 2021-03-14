42b9c4fb1fa62e540c10d1986c31ee31c043391f
artephakt-studio <artephakt.studio@gmail.com>
2021-03-14 13:58:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-v-for-object
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-v-for-object)
---
+++ index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import './style.css';
+
+new Vue({
+  el: '#app',
+  data: {
+    personne: {
+      prenom: 'Vincent',
+      nom: 'LEBRUN',
+      age: 39
+    }
+  }
+});
